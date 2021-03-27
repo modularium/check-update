5761af73720bb1d7dfbae24bdd45d917b652307d
redcarti <ppamperr@gmail.com>
2021-03-27 16:39:34 +0500

    !publish updated config, dist

--- dist/index.js
+++ dist/index.js
@@ -1,111 +1,45 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = __importDefault(require("axios"));
-var NoSuchError = /** @class */ (function (_super) {
-    __extends(NoSuchError, _super);
-    function NoSuchError(message, code) {
-        var _this = _super.call(this) || this;
-        _this.message = message;
-        _this.code = code;
-        return _this;
+const axios_1 = __importDefault(require("axios"));
+class NoSuchError extends Error {
+    constructor(message, code) {
+        super();
+        this.message = message;
+        this.code = code;
     }
-    return NoSuchError;
-}(Error));
+}
 /**
  * Checks if a package needs to be updated.
  *
  * @async
  */
-var checkUpdate = function (name, version, registry) {
-    if (registry === void 0) { registry = 'https://registry.npmjs.org'; }
-    return __awaiter(void 0, void 0, void 0, function () {
-        var response, versions, versionsKeys, lastVersionIndex, versionToCheckIndex, lastVersion, e_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 2, , 3]);
-                    return [4 /*yield*/, axios_1.default.get(registry + "/" + name, {
-                            validateStatus: function (status) { return status === 200; },
-                        })];
-                case 1:
-                    response = _a.sent();
-                    versions = response.data.versions;
-                    if (!versions[version])
-                        throw new NoSuchError('There\'s no such a version!', 'noSuchVersion');
-                    versionsKeys = Object.keys(versions);
-                    lastVersionIndex = versionsKeys.length - 1;
-                    versionToCheckIndex = versionsKeys.indexOf(version);
-                    lastVersion = versionsKeys[lastVersionIndex];
-                    return [2 /*return*/, {
-                            lastVersion: lastVersion,
-                            isNeeded: lastVersionIndex > versionToCheckIndex
-                        }];
-                case 2:
-                    e_1 = _a.sent();
-                    if (e_1.response && e_1.response.status === 404) {
-                        throw new NoSuchError('There\'s no such a package!', 'noSuchPackage');
-                    }
-                    else {
-                        throw e_1;
-                    }
-                    return [3 /*break*/, 3];
-                case 3: return [2 /*return*/];
-            }
+const checkUpdate = async (name, version, registry = 'https://registry.npmjs.org') => {
+    try {
+        const response = await axios_1.default.get(`${registry}/${name}`, {
+            validateStatus: (status) => status === 200,
         });
-    });
+        const { versions } = response.data;
+        if (!versions[version])
+            throw new NoSuchError('There\'s no such a version!', 'noSuchVersion');
+        const versionsKeys = Object.keys(versions);
+        const lastVersionIndex = versionsKeys.length - 1;
+        const versionToCheckIndex = versionsKeys.indexOf(version);
+        const lastVersion = versionsKeys[lastVersionIndex];
+        return {
+            lastVersion,
+            isNeeded: lastVersionIndex > versionToCheckIndex
+        };
+    }
+    catch (e) {
+        if (e.response && e.response.status === 404) {
+            throw new NoSuchError('There\'s no such a package!', 'noSuchPackage');
+        }
+        else {
+            throw e;
+        }
+    }
 };
 exports.default = checkUpdate;
